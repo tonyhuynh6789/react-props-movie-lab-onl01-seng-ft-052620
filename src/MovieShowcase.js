2ef17d3488e0149b20d17366e9896f497be963ec
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2020-10-02 12:36:33 -0700

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,6 +6,7 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+    return movieData.map(item => <MovieCard title={item.title}  IMDBRating={item.IMDBRating} genres={item.genres} poster={item.poster} />)
   }
 
   render() {
--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -30,14 +30,21 @@ const posterMap = {
 export default class MovieCard extends Component {
 
   render() {
+    console.log(this.props)
     return (
       <div className="movie-card">
         {/* which component should receive which props? */}
-        <CardFront />
-        <CardBack />
+        <CardFront url={posterMap[this.props.poster]}/>
+        <CardBack genres={this.props.genres} title={this.props.title} IMDBRating={this.props.IMDBRating} />
       </div>
     )
   }
 }
 
 // Don't forget your default props!
+MovieCard.defaultProps = {
+  title: 'Unknown',
+  poster: 'default',
+  genres: ["No Genre(s) Found"],
+  IMDBRating: null
+}
